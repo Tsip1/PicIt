487cf4b0ef60f085160294d3baf0a5dcc47ebef1
AK2PickIt <kowler@g.jct.ac.il>
2017-11-27 22:07:35 +0200

    update 11.27.2017

---
+++ app/src/main/java/com/example/kowler/pickit2/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.kowler.pickit2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+//Go to camera
+        findViewById(R.id.buttonCamera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivityForResult(intent, 0);
+            }
+        });
+ //end of camera button
+//Go to gallery
+        findViewById(R.id.buttonGallery).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+// getting the selected picture
+                Intent intent = new Intent();
+// Show only images, no videos or anything else
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                // Always show the chooser (if there are multiple options available)
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+            }
+        });
+//end of gallery button
+
+    }      //end of on create
+    int PICK_IMAGE_REQUEST = 1;
+
+
+}
